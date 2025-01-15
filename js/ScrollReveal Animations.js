352941daf6549e33870cc384180d58120e0b2a9d
synchoro <zhouanqi465@gmail.com>
2025-01-15 23:53:27 +0900

    zhou

---
+++ js/ScrollReveal Animations.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 400,
+});
+
+sr.reveal(".header");
+sr.reveal(".navbar");
+sr.reveal(".container", { interval: 200 });
+sr.reveal(".about-me", { interval: 200 });
+sr.reveal(".part-time-job", { interval: 200 });
+sr.reveal(".hobbies", { interval: 200 });
+sr.reveal(".strength", { interval: 200 });
+sr.reveal(".works", { interval: 200 });
+sr.reveal(".footer", { interval: 200 });
